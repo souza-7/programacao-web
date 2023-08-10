382c2647adf37b6653a7884a89f3c015f15eef67
souza-7 <84939200+souza-7@users.noreply.github.com>
2023-08-10 11:14:49 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,19 @@
+var num1 = 69;
+var num2 = 24;
+var soma;
+var subtracao;
+var multipicacao;
+var divisao;
+
+num1= Number(num1);
+num2= Number(num2);
+
+soma = num1 + num2;
+subtracao = num1 - num2;
+multipicacao = num1 * num2;
+divisao = num1 / num2;
+
+console.log (`Resultado da soma Entre ${num1} e ${num2} é ${soma}`);
+console.log (`Resultado da subtração Entre ${num1} e ${num2} é ${subtracao}`);
+console.log (`Resultado da multiplicação Entre ${num1} e ${num2} é ${multipicacao}`);
+console.log (`Resultado da divisão Entre ${num1} e ${num2} é ${divisao}`);
